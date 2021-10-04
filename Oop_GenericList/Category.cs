d86abb66ba2f0a789427dca16988bec1036d1f0a
nurbilgekul <90116916+nurbilgekul@users.noreply.github.com>
2021-10-04 16:10:40 +0300

    Add files via upload

---
+++ Oop_GenericList/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oop_GenericList
+{
+    //generics defining class or methods in any data type,
+    //when define generic dont know the paramaters until called or instantiated
+
+    public  class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        
+
+    }
+    
+}
---
+++ Oop_GenericList/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Oop_GenericList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            List<double> kategoriler = new List<double>() { };
+
+            List<TextBox> texts = new List<TextBox>();
+        
+
+            List<Category> categories = new List<Category>()
+            {
+                new Category { Id = 1, Name = "Watch", Description = "8.Generation Watch" },
+                new Category { Id = 2, Name = "Phone", Description = "Good Choice" },
+                new Category { Id = 3, Name = "Laptop", Description = "Fastest" }
+            };
+            dataGridView1.DataSource = categories.ToList();
+
+
+
+            
+        }
+
+       
+    }
+}
